4834cab12a738fd1723ae500386db38f7a2ca324
Anel Husakovic <anelhusakovic@gmail.com>
2019-05-12 16:03:24 -0700

    Pass dictonary to template and create namespace directory tamplates

--- django/my_projects/mysite_fligths/_fligths_app/views.py
+++ django/my_projects/mysite_fligths/_fligths_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Flight
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello !")
+    template_name="_flights_app/index.html"
+    context={
+        "flights":Flight.objects.all()
+    } #context dictionary passes keys and values
+    return render(request, template_name, context)
