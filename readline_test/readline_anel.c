bda3fd5f53cfcb95b7daa09d5823ec331148fd5e
Anel Husakovic <anel@mariadb.org>
2019-05-18 14:27:35 -0700

    Add completition example of readline

--- readline_test/readline_anel.c
+++ readline_test/readline_anel.c
@@ -19,7 +19,7 @@ rl_gets ()
     }
 
   /* Get a line from the user. */
-  line_read = readline ("");
+  line_read = readline ("anel2> ");
 
   /* If the line has any text in it,
      save it on the history. */
@@ -30,8 +30,78 @@ rl_gets ()
 }
 
 
+  
+/* Invert the case of the COUNT following characters. */
+int
+invert_case_line (count, key)
+     int count, key;
+{
+  register int start, end, i, direction;
+
+  start = rl_point;
+
+  if (rl_point >= rl_end)
+    return (0);
+
+  if (count < 0)
+    {
+      direction = -1;
+      count = -count;
+    }
+  else
+    direction = 1;
+      
+  /* Find the end of the range to modify. */
+  end = start + (count * direction);
+
+  /* Force it to be within range. */
+  if (end > rl_end)
+    end = rl_end;
+  else if (end < 0)
+    end = 0;
+
+  if (start == end)
+    return (0);
+
+  if (start > end)
+    {
+      int temp = start;
+      start = end;
+      end = temp;
+    }
+
+  /* Tell readline that we are modifying the line,
+     so it will save the undo information. */
+  rl_modifying (start, end);
+
+  for (i = start; i != end; i++)
+    {
+      if (_rl_uppercase_p (rl_line_buffer[i]))
+        rl_line_buffer[i] = _rl_to_lower (rl_line_buffer[i]);
+      else if (_rl_lowercase_p (rl_line_buffer[i]))
+        rl_line_buffer[i] = _rl_to_upper (rl_line_buffer[i]);
+    }
+  /* Move point to on top of the last character changed. */
+  rl_point = (direction == 1) ? end - 1 : start;
+  return (0);
+}
+
+
 int main()
 {
-	printf("HI");
+  printf("%s \n", readline("anel> "));
+  printf("%s \n", rl_gets ());
+  int tmp=0;
+  char *in;
+  while(tmp<10)
+  {
+    in=readline("Enter> ");
+    add_history(in);
+    printf("%s \n", in);
+
+    rl_redisplay();
+    free(in);
+    tmp++;
+  }
 	return 0;
 }
